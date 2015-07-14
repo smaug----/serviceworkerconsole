c07e9fed0793104f9cf68932f2f43493ef4cd691
smaug <smaug@localhost.localdomain>
2015-07-15 00:54:17 +0300

    initial

---
+++ sw.js
@@ -0,0 +1,17 @@
+(function() {
+   addEventListener("message",
+                    function(e) {
+                      e.source.postMessage(eval(e.data) + "");
+                    },
+                    true);
+})();
+
+// Hack. Apparently this should keep the ServiceWorker alive.
+(function() {
+   addEventListener("install",
+                    function(e) {
+                      e.waitUntil(new Promise(function() {}));
+                    },
+                    true);
+})();
+
